c4c13877ad6a08755cd7051506f30008a44af798
杨嘉俊 <59992182+Justin116@users.noreply.github.com>
2021-01-07 20:44:05 +0800

    Update Files and Skills
    Upload:
    - baidu.svg
    - github.svg
    - index.md ( for explaining the problems in the html )
    
    Update:
    - index.html: add buttons for choosing the searching engine
    - index.css: add styles of engine-choosing buttons
    - index.js: add functions of changing colors while clicking the button and set the another one's color back
    
    Next Update Plan:
    - add skills of searching in different searching engine while choosing different buttons
    - set skills of changing theme (dark/light)

--- index/index.js
+++ index/index.js
@@ -1,15 +1,63 @@
+// Set background color while the button is clicked
+
+var icon = document.querySelectorAll('.iconBackground')
+var svg = document.querySelectorAll('.icon')
+console.log(icon);
+console.log(svg);
+
+
+for (let i = 0; i < icon.length; i++) {
+  icon[i].addEventListener('click', function (e) { //给所有按钮添加监听事件，监听点击事件
+    console.log('所点击元素', this);
+    console.log('所点击元素的子元素', this.children[0]); //所点击元素的子元素
+
+    e.stopPropagation() //清除事件冒泡/捕获
+
+    clearAllColor()
+    // 被点击的元素背景色改变
+    this.style.backgroundColor = '#fff'
+    this.children[0].style.fill = '#000'
+  })
+}
+
+for (let i = 0; i < svg.length; i++) {
+  svg[i].addEventListener('click', function (e) { //给所有按钮添加监听事件，监听点击事件
+    e.stopPropagation() //清除事件冒泡/捕获
+    console.log('所点击元素', this);
+    console.log('所点击元素的父元素', this.parentNode); //所点击元素的父元素
+
+    clearAllColor()
+    // 被点击的图片
+    this.style.fill = '#000'
+    this.parentNode.style.backgroundColor = '#fff'
+  })
+}
+
+
+// 清除所有颜色
+function clearAllColor () {
+  for (let i = 0; i < icon.length; i++) {
+    icon[i].style.backgroundColor = ''
+  }
+  for (let i = 0; i < svg.length; i++) {
+    svg[i].style.fill = ''
+  }
+}
+
 function searchBaidu() {
   var searchElement;
   searchElement = document.getElementsByClassName("searchBox")[0];
   var searchTitle;
   searchTitle = searchElement.value;
-  console.log(searchTitle);
   if (window.event.keyCode == 13) {
-    window.location.href = `https://www.baidu.com/#ie={inputEncoding}&wd=${searchTitle}`
+    window.open(`https://github.com/search?q=${searchTitle}`);
+    window.open(`https://www.baidu.com/#ie={inputEncoding}&wd=${searchTitle}`);
   }
-  
-  //If searchTitle != "", unblur
+
+  // If searchTitle != "", unblur
   if (searchTitle != "") {
     searchElement.setAttribute('style', 'filter: blur(0)');
   }
+
+  // Check style and set href
 }
